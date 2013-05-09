410bcdc47ab117f445083dafbba9019e1fef21d8
Justin Aquadro <jaquadro@gmail.com>
2013-05-09 00:29:33 -0400

    Fix crash on creating new project

--- Treefrog/Presentation/MinimapPresenter.cs
+++ Treefrog/Presentation/MinimapPresenter.cs
@@ -91,8 +91,10 @@ namespace Treefrog.Presentation
 
         private void InitializeLayers ()
         {
-            foreach (Layer layer in _levelPresenter.Level.Layers)
-                AddLayer(layer);
+            if (_levelPresenter != null) {
+                foreach (Layer layer in _levelPresenter.Level.Layers)
+                    AddLayer(layer);
+            }
         }
 
         private void ClearLayers ()
@@ -126,12 +128,12 @@ namespace Treefrog.Presentation
 
         public TexturePool TexturePool
         {
-            get { return _levelPresenter.TexturePool; }
+            get { return _levelPresenter != null ? _levelPresenter.TexturePool : null; }
         }
 
         public Level Level
         {
-            get { return _levelPresenter.Level; }
+            get { return _levelPresenter != null ? _levelPresenter.Level : null; }
         }
 
         public event EventHandler CurrentLevelChanged;
@@ -147,7 +149,7 @@ namespace Treefrog.Presentation
 
         public void UpdateAreaBox ()
         {
-            if (_levelPresenter.LevelGeometry != null) {
+            if (_levelPresenter != null && _levelPresenter.LevelGeometry != null) {
                 _boxAnnot.Start = _levelPresenter.LevelGeometry.VisibleBounds.Location;
                 _boxAnnot.End = new Point(_levelPresenter.LevelGeometry.VisibleBounds.Right, _levelPresenter.LevelGeometry.VisibleBounds.Bottom);
             }
